78da538ab5b10cbaa4f8e2a9e64bfea0ca5c2d57
Benjamin Carter <benjamincarter@caresolutions.com>
2024-03-12 12:43:36 -0400

    Resolved Deprecation of waitForTimeout(3000)

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,12 +1,24 @@
 const puppeteer = require('puppeteer')
+const {setTimeout} = require("node:timers/promises");
 
 describe('My first Puppeteer test', function() {
 
-    it('shold launch the browser', async function() {
-        const browser = await puppeteer.launch({ headless: false})  //True runs the bowser in headless mode
+    it('should launch the browser', async function() {
+        //const browser = await puppeteer.launch({ headless: false})  //True runs the bowser in headless mode
+        //const browser = await puppeteer.launch({ headless: false, slowMo: 500 })  //True runs the bowser in headless mode and in slowMo mode
+        //const browser = await puppeteer.launch({ headless: true, slowMo: 500 })  //True runs the bowser silently in headless mode and in slowMo mode
+        //const browser = await puppeteer.launch({ headless: false, slowMo: 10, devtools: true })  //True runs the bowser with console dev tools opened.
+        const browser = await puppeteer.launch({ headless: false, slowMo: 10, devtools: false })  //True runs the bowser with console dev tools closed.
+
         const page =     await browser.newPage()
 
         await page.goto('http://example.com/')
+
+        // Pause for three seconds              << doesn't work
+        //await page.waitForTimeout(3000)       << doesn't work
+        //await page.waitFor(3000)              << doesn't work
+        //waitFor(3000)                         << doesn't work
+        await setTimeout(3000);
 
         //Check if element is visible like h1 tag
 
@@ -17,3 +29,4 @@ describe('My first Puppeteer test', function() {
 
     })
 })
+
